0a49063eabea2f602c3eaab7979781aeb4a28696
unknown <cu.18bcs6591@gmail.com>
2023-12-19 01:00:51 +0530

    Implemented redux-toolkit

---
+++ redux-toolkit-tutorial/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <>
+      <AddTodo />
+      <Todo />
+    </>
+  );
+}
+
+export default App;
